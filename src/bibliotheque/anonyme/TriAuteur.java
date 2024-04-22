87b30c61120c3968ef244b68f24e4d67f96c0259
Colbern <urbainmatthew64@gmail.com>
2024-04-22 14:22:54 +0200

    suite class anonyme

---
+++ src/bibliotheque/anonyme/TriAuteur.java
@@ -0,0 +1,31 @@
+package bibliotheque.anonyme;
+
+import bibliotheque.metier.Ouvrage;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TriAuteur {
+public static void main(String args[]){
+
+         //tri sur l'age
+         Collections.sort(lemp, new Comparator<Ouvrage>(){ //au lieu de new AgeComparator
+
+
+        //tri sur le nom
+         Collections.sort(, new Comparator<Ouvrage>()
+
+
+         public int compare(Ouvrage ouv1,Ouvrage ouv2){
+                        String ouv1Titre = ouv1.getTitre();
+                        String ouv2Titre = ouv2.getTitre();
+                         return ouv1Titre.compareTo(ouv2Titre);
+                         }
+ }
+                 );
+
+         System.out.println("\n\nOrdre des employes apres tri sur le nom");
+                 System.out.println(lemp);
+
+    }
+}
--- src/bibliotheque/anonyme/TriRayon.java
+++ src/bibliotheque/anonyme/TriRayon.java
@@ -3,30 +3,27 @@ package bibliotheque.anonyme;
 import bibliotheque.metier.Ouvrage;
 import bibliotheque.metier.Rayon;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class TriRayon {
 public static void main(String args[]){
 
-         //tri sur l'age
-         Collections.sort(lemp, new Comparator<Ouvrage>(){ //au lieu de new AgeComparator
-
-
-        //tri sur le nom
-         Collections.sort(, new Comparator<Ouvrage>()
+    ArrayList<Rayon> lray = new ArrayList<>();
+         Collections.sort(lray, new Comparator<Rayon>());
 
 
          public int compare(Rayon ray1, Rayon ray2){
-                        String ouv1Titre = ouv1.getTitre();
-                        String ouv2Titre = ouv2.getTitre();
-                         return ouv1Titre.compareTo(ouv2Titre);
+                        String ray1Code = ray1.getCodeRayon();
+                        String ray2Code = ray2.getCodeRayon();
+                         return ray1Code.compareTo(ray2Code);
                          }
  }
                  );
 
-         System.out.println("\n\nOrdre des employes apres tri sur le nom");
-                 System.out.println(lemp);
+         System.out.println("\n\nOrdre des rayon apres tri sur le code");
+                 System.out.println(lray);
 
     }
 }
